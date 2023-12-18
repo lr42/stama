fe35904e5bc7ea452b885a9e6347fb0d349e7aef
LR <noreply_719@symfu.com>
2023-12-17 19:01:40 -0500

    Add `Callable` type hints to assignable functions
    I could've sworn that I did this earlier.  But apparently not?  Oh well,
    it's done now.

--- stama/__init__.py
+++ stama/__init__.py
@@ -4,6 +4,7 @@ import logging
 from typing import (
     List,
     Optional,
+    Callable,
 )
 from threading import RLock
 
@@ -33,13 +34,13 @@ class Event:  # pylint: disable=too-few-public-methods
 
         self._description = description
 
-        self.on_before_transition = lambda: logger.debug(
+        self.on_before_transition: Callable = lambda: logger.debug(
             "No action set for before %s transition.", self
         )
-        self.on_during_transition = lambda: logger.debug(
+        self.on_during_transition: Callable = lambda: logger.debug(
             "No action set for during %s transition.", self
         )
-        self.on_after_transition = lambda: logger.debug(
+        self.on_after_transition: Callable = lambda: logger.debug(
             "No action set for after %s transition.", self
         )
 
@@ -74,13 +75,13 @@ class State:
 
         self.transitions: dict[Event, "State"] = {}
 
-        self.on_entry = lambda: logger.debug(
+        self.on_entry: Callable = lambda: logger.debug(
             "No action set for entering %s.", self
         )
-        self.on_exit = lambda: logger.debug(
+        self.on_exit: Callable = lambda: logger.debug(
             "No action set for exiting %s.", self
         )
-        self.enforce = lambda: logger.debug(
+        self.enforce: Callable = lambda: logger.debug(
             "Nothing to enforce on %s.", self
         )
 
@@ -164,7 +165,7 @@ class StateMachine:
         self._starting_state = starting_state
         self._current_state = self._starting_state
 
-        self.enforce = lambda: logger.debug(
+        self.enforce: Callable = lambda: logger.debug(
             "Nothing to enforce on %s.", self
         )
 
